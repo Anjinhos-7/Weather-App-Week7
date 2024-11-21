73979a76824e970a880641c0874f36fe3034ffbf
Anjinhos7 <118299137+Anjinhos7@users.noreply.github.com>
2024-11-21 18:37:58 +0000

    New updates made
    On HTML file- added ids to all the objects that will be affected on the script file, that did not have ids yet, such as humidity or icon.
    
    On script file- made sure that description, the time, humidity, wind speed, and the icon changed depending on the conditions and the city searched.
    Also added another function to make sure that the initial page has a city already searched so that those areas of temperature are not left blank, like the HTML file is.

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
-function search(event) {
-  event.preventDefault();
-  let searchInputElement = document.querySelector("#search-input");
-  let city = searchInputElement.value;
+function displayTemperature(response) {
+  let temperature = Math.round(response.data.temperature.current);
+  let temperatureElement = document.querySelector(".current-temperature-value");
+  temperatureElement.innerHTML = `${temperature}`;
 
-  let apiKey = "2394045tebc4f1e4ea3820407e9abod9";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  let cityName = response.data.city;
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = cityName;
 
-  axios.get(apiUrl).then(displayTemperature);
+  let description = document.querySelector("#description");
+  let humidity = document.querySelector("#humidity");
+  let windSpeed = document.querySelector("#wind-speed");
+  let time = document.querySelector("#current-date");
+
+  description.innerHTML = response.data.condition.description;
+  humidity.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeed.innerHTML = `${response.data.wind.speed}km/h`;
+
+  let date = new Date(response.data.time * 1000);
+  let icon = document.querySelector("#icon");
+
+  time.innerHTML = formatDate(date);
+  icon.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
 }
-
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", search);
 
 function formatDate(date) {
   let minutes = date.getMinutes();
   let hours = date.getHours();
-  let day = date.getDay();
-
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-
   let days = [
     "Sunday",
     "Monday",
@@ -34,22 +35,33 @@ function formatDate(date) {
     "Friday",
     "Saturday",
   ];
+  let day = days[date.getDay()];
 
-  let formattedDay = days[day];
-  return `${formattedDay} ${hours}:${minutes}`;
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
-let currentDateELement = document.querySelector("#current-date");
-let currentDate = new Date();
-currentDateELement.innerHTML = formatDate(currentDate);
+function search(city) {
+  let apiKey = "2394045tebc4f1e4ea3820407e9abod9";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
-function displayTemperature(response) {
-  let temperature = Math.round(response.data.temperature.current);
-  let cityName = response.data.city;
-
-  let temperatureElement = document.querySelector(".current-temperature-value");
-  let cityElement = document.querySelector("#current-city");
-
-  cityElement.innerHTML = cityName;
-  temperatureElement.innerHTML = `${temperature}`;
+  axios.get(apiUrl).then(displayTemperature);
 }
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  search(searchInput.value);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSearchSubmit);
+
+search("Paris");
